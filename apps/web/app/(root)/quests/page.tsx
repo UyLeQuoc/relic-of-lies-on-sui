36598ada81b235d31380ccf719a913a901d3315e
ngogiahuandev <ngogiahuan23122003@gmail.com>
2026-01-17 11:35:30 +0700

    change from sidebar to header

--- apps/web/app/(root)/quests/page.tsx
+++ apps/web/app/(root)/quests/page.tsx
@@ -1,17 +1,11 @@
-'use client';
-
 import Footer from "@/components/common/footer";
-import { Sidebar } from "@/components/common/sidebar";
 import { QuestsContent } from "./_components/quests-content";
 
 export default function QuestsPage() {
 	return (
-		<div className="flex">
-			<Sidebar />
-			<div className="flex-1">
-				<QuestsContent />
-				<Footer />
-			</div>
-		</div>
+		<>
+			<QuestsContent />
+			<Footer />
+		</>
 	);
 }
--- apps/web/app/(root)/rooms/page.tsx
+++ apps/web/app/(root)/rooms/page.tsx
@@ -1,17 +1,11 @@
-'use client';
-
 import Footer from "@/components/common/footer";
-import { Sidebar } from "@/components/common/sidebar";
 import { RoomsLobby } from "./_components/rooms-lobby";
 
 export default function RoomsPage() {
 	return (
-		<div className="flex">
-			<Sidebar />
-			<div className="flex-1">
-				<RoomsLobby />
-				<Footer />
-			</div>
-		</div>
+		<>
+			<RoomsLobby />
+			<Footer />
+		</>
 	);
 }
